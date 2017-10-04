89cb24f15399027ed93f2e45568d0ffb527574a3
fastict <fast@ict.usc.edu>
2017-10-04 11:32:37 -0700

    added main NVBG application

---
+++ NVBG/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace NVBG
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            List<string> commandLineArguments = new List<string>(args);
+            Application.Run(new NVBGForm(commandLineArguments));
+        }
+    }
+}
+
